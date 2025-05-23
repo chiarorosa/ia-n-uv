d9c02fa8e9a971c4bea9101fa604e941ec8e6135
chiarorosa <pablo.chiaro.rosa@gmail.com>
2025-05-23 20:17:09 -0300

    feat: utils

--- src/ia_n_uv/main.py
+++ src/ia_n_uv/main.py
@@ -1,8 +1,21 @@
 import sys
 
+from .utils import parse_args
+
+# Entry point do pacote
+
 
 def main():
     print("Meu pacote python começa aqui!")
+    args = parse_args()
+    try:
+        # url = args.url
+        problema = args.problema
+        print(f"Problema: {problema}")
+        return 0
+    except Exception as e:
+        print(f"Erro: {e}")
+        return 1
 
 
 if __name__ == "__main__":
---
+++ src/ia_n_uv/utils.py
@@ -0,0 +1,18 @@
+# Utilitários do pacote
+import argparse
+
+"""
+    parse_args
+    Função para parsear os argumentos da linha de comando  
+"""
+
+
+def parse_args():
+    parser = argparse.ArgumentParser(description="Meu pacote python xyz")
+
+    parser.add_argument(
+        "problema",
+        help="Problema a ser resolvido",
+    )
+
+    return parser.parse_args()
