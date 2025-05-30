f7dc814497f7d59f490323d037cc56310ff905da
chiarorosa <pablo.chiaro.rosa@gmail.com>
2025-05-30 20:23:09 -0300

    feat:llm

--- src/ia_n_uv/main.py
+++ src/ia_n_uv/main.py
@@ -1,6 +1,7 @@
 import sys
 
 from .utils import parse_args
+from .algoritmos.gemini_integration import GeminiClient
 
 # Entry point do pacote
 
@@ -12,6 +13,14 @@ def main():
         # url = args.url
         problema = args.problema
         print(f"Problema: {problema}")
+        
+        print(f"Geração de Texto básico")
+        gemini_client = GeminiClient(api_key='tua-chave', model_name='gemini-2.0-flash', max_output_tokens=500)
+        response_text = gemini_client.generate_response(problema)
+        
+        print(f"resposta: {response_text}\n")
+        
+        
         return 0
     except Exception as e:
         print(f"Erro: {e}")
